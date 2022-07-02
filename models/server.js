8feda574678bbd8c8e0a99de320d6de3ca542dac
CJBarahona <cbarahona@t-developing.com>
2022-07-02 15:06:13 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        this.middlewares();
+
+        this.routes();
+    }
+
+    middlewares() {
+
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+
+    }
+
+    routes() {
+
+        this.app.use(this.usuariosPath, require('../routes/usuarios'))
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        })
+    }
+}
+
+module.exports = Server;
